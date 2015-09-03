9f904d198a274cc1714bdc74e5bcfd0963556826
Sebastian Stephan <sstephan1337@gmail.com>
2015-09-03 14:20:30 +0200

    Flush and add user as default behavior (only for dev)

--- run_web.sh
+++ run_web.sh
@@ -3,4 +3,9 @@
 bower install --allow-root
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Flush everytime and create user
+python manage.py flush --noinput
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+
 uwsgi --socket :8000 --module probr.wsgi_django:application --enable-threads
